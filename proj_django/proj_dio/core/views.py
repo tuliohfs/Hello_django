6ce4987bd57c2446fcfd104e1163be7fbf59313e
Tulio Henrique <tulio.henrique@liv.local>
2023-11-28 11:06:45 -0300

    Commit de atividade das rotas de calculos

--- proj_django/proj_dio/core/views.py
+++ proj_django/proj_dio/core/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello(request):
     return HttpResponse('Hello Word')
+
+def soma(request, n1, n2):
+    
+    total = int(n1) + int(n2)
+    return HttpResponse("A soma de {} e {} e igual {}".format(n1, n2, total))
+    
+def subtracao(request, n1, n2):
+    
+    total = int(n1) - int(n2)
+    return HttpResponse("A Subtração de {} e {} e igual {}".format(n1, n2, total))
+
+def multiplicacao(request, n1, n2):
+    
+    total = int(n1) * int(n2)
+    return HttpResponse("A Multiplicação de {} e {} e igual {}".format(n1, n2, total))
+
+def divisao(request, n1, n2):
+    
+    total = int(n1) / int(n2)
+    return HttpResponse("A Divisão de {} e {} e igual {}".format(n1, n2, total))
--- proj_django/proj_dio/proj_dio/urls.py
+++ proj_django/proj_dio/proj_dio/urls.py
@@ -4,5 +4,9 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello)
+    path('hello/', views.hello),
+    path('soma/<n1>/<n2>', views.soma),
+    path('subtracao/<n1>/<n2>', views.subtracao),
+    path('multiplicacao/<n1>/<n2>', views.multiplicacao),
+    path('divisao/<n1>/<n2>', views.divisao)
 ]
